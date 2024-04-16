4aa1dd0c4ab1ec71f0489de954e2af4e65b01bb9
julian <julian@cheerfy.com>
2024-04-16 12:13:24 +0200

    Add gi

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,6 +3,7 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, otps)
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
