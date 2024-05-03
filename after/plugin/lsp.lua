dab8d47bad56abe58592cb863f7fddbe1976a53b
julian <julian@cheerfy.com>
2024-05-03 14:01:39 +0200

    Fix lsp automplete shortcuts

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -35,27 +35,18 @@ require('mason-lspconfig').setup({
 -- Autocompletion config
 ---
 local cmp = require('cmp')
-local cmp_action = lsp_zero.cmp_action()
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
+local cmp_mappings = lsp_zero.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
 
-    -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
 
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-
-    -- Scroll up and down in the completion documentation
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  }),
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
+lsp_zero.setup_nvim_cmp({
+  mapping = cmp_mappings
 })
