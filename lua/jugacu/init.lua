c8d4da5fd18df711b28b1a5af4105e7639877a9e
julian <julian@cheerfy.com>
2024-06-19 16:53:37 +0200

    Move to eslint_d

--- lua/jugacu/init.lua
+++ lua/jugacu/init.lua
@@ -44,6 +44,8 @@ Plug('hrsh7th/nvim-cmp')
 Plug('hrsh7th/cmp-nvim-lsp')
 Plug('L3MON4D3/LuaSnip')
 Plug ('VonHeikemen/lsp-zero.nvim', {['branch']= 'v3.x'})
+Plug('nvimtools/none-ls.nvim')
+Plug('nvimtools/none-ls-extras.nvim')
 
 -- Misc
 Plug('tpope/vim-fugitive')
