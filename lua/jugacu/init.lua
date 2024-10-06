19ecf4dd1fe18273afbc307c0b468bd639ca71d8
julian <julian@cheerfy.com>
2024-10-06 17:33:29 +0200

    Add debugging

--- lua/jugacu/init.lua
+++ lua/jugacu/init.lua
@@ -18,7 +18,7 @@ autocmd('TextYankPost', {
     end,
 })
 
-autocmd({"BufWritePre"}, {
+autocmd({ "BufWritePre" }, {
     group = JugacuGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
@@ -33,8 +33,12 @@ Plug('rose-pine/neovim')
 
 -- Telescope
 Plug('nvim-lua/plenary.nvim')
-Plug('nvim-telescope/telescope.nvim', { ['tag'] ='0.1.6' })
-Plug('nvim-telescope/telescope-fzf-native.nvim', { ['do'] = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' })
+Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.6' })
+Plug('nvim-telescope/telescope-fzf-native.nvim',
+    {
+        ['do'] =
+        'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    })
 
 -- LSP
 Plug('williamboman/mason.nvim')
@@ -43,9 +47,16 @@ Plug('neovim/nvim-lspconfig')
 Plug('hrsh7th/nvim-cmp')
 Plug('hrsh7th/cmp-nvim-lsp')
 Plug('L3MON4D3/LuaSnip')
-Plug ('VonHeikemen/lsp-zero.nvim', {['branch']= 'v3.x'})
+Plug('VonHeikemen/lsp-zero.nvim', { ['branch'] = 'v3.x' })
 Plug('nvimtools/none-ls.nvim')
 Plug('nvimtools/none-ls-extras.nvim')
+
+-- DEBUGGING
+Plug('jay-babu/mason-nvim-dap.nvim')
+Plug('mfussenegger/nvim-dap')
+Plug('leoluz/nvim-dap-go')
+Plug('nvim-neotest/nvim-nio')
+Plug('rcarriga/nvim-dap-ui')
 
 -- Misc
 Plug('tpope/vim-fugitive')
@@ -53,7 +64,7 @@ Plug('airblade/vim-gitgutter')
 Plug('mbbill/undotree')
 Plug('theprimeagen/harpoon')
 
-Plug('nvim-treesitter/nvim-treesitter', {['do']= ':TSUpdate'})
+Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
 Plug('nvim-treesitter/nvim-treesitter-context')
 
 vim.call('plug#end')
